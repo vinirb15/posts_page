c2e5911d9aedb89c99bc9c5bb2a918c7a0107645
vbueno <viniciusbueno15@gmail.com>
2020-10-21 18:53:13 -0300

    controller

--- backend/src/controllers/MessageController.js
+++ backend/src/controllers/MessageController.js
@@ -3,7 +3,61 @@ const knex = require('../database/index');
 
 module.exports = {
     async index(req, res) {
-        const results = await knex('posts');
-        return res.json(results);
-    }
+        const results = await knex('posts')
+        return res.json(results)
+    },
+
+    async single(req, res, next) {
+        const { id } = req.params
+        try {
+            const results = await knex('posts')
+                .select('*')
+                .where('id', id)
+            return res.json(results)
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    async create(req, res, next) {
+        const { message } = req.body
+
+        try {
+            await knex('posts')
+            .insert({
+                message
+            })
+            return res.status(201).send()
+
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    async comments(req, res) {
+        const { id } = req.params
+
+        const results = await knex('comments')
+        .join('posts', 'posts.id', '=', 'comments.postId')
+        .select('comments.*')
+        .where('posts.id', id)
+        return res.json(results)
+    },
+
+    async createMenssage(req, res, next) {
+        const { id } = req.params
+        const { comment } = req.body
+
+        try {
+            await knex('comments')
+            .insert({
+                postId: id,
+                comment
+            })
+            return res.status(201).send()
+
+        } catch (error) {
+            next(error)
+        }
+    },
 }
