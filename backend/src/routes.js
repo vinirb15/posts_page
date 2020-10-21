66439d71491d564b0f8f5f9c4a2f234ba27b4118
vbueno <viniciusbueno15@gmail.com>
2020-10-21 18:53:08 -0300

    routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,5 +4,10 @@ const routes = express.Router();
 const MessageController = require('./controllers/MessageController');
 
 routes.get('/posts', MessageController.index);
+routes.get('/posts/:id', MessageController.single);
+routes.post('/posts', MessageController.create);
+routes.get('/posts/:id/comments', MessageController.comments);
+routes.post('/posts/:id/comments', MessageController.createMenssage);
+
 
 module.exports = routes;
