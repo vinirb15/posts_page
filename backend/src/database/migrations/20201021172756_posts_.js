4ce3a777ab994ff169eedd71541c678b839e0aee
vbueno <viniciusbueno15@gmail.com>
2020-10-21 18:42:19 -0300

    migrate up

--- backend/src/database/migrations/20201021172756_posts_.js
+++ backend/src/database/migrations/20201021172756_posts_.js
@@ -1,11 +1,15 @@
 
 exports.up = function (knex) {
     return knex.schema.createTable('posts', function (table) {
-        table.string('id').primary()
-        table.text('message').notNullable()
-        table.string('postId').notNullable()
-        table.string('comment').notNullable()
-    })
+        table.increments('id').primary()
+        table.string('message', 253).notNullable()
+    }).then(function () {
+        return knex.schema.createTable('comments', function (table) {
+            table.increments('id').primary();
+            table.string('comment', 253).notNullable();
+            table.integer('postId', 20).unsigned().references('id').inTable('posts');
+        });
+    });
 };
 
 exports.down = function (knex) {
