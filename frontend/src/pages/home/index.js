6aa46831cca5bb989064cd2f00f4fec67930cc71
vbueno <viniciusbueno15@gmail.com>
2020-10-22 13:36:02 -0300

    primeira pag

---
+++ frontend/src/pages/home/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
+import './styles.css'
+
+export default class Home extends Component {
+
+    state = {
+        posts: [],
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    loadPosts = async () => {
+        const response = await api.get('/posts')
+        this.setState({posts: response.data})
+    }
+
+    render() {
+        const { posts } = this.state;
+
+        return (
+            <div className="posts-list" >
+                <div className="containerUtils">
+                    <input
+                    />
+                </div>
+
+                {posts.map(post => (
+                    <article key={post.id}>
+                        <h2>{post.message}</h2>
+                        <Link to={`/posts/${post.id}`}>Acessar</Link>
+                    </article>
+                ))}
+
+            </div >
+        )
+    }
+}
