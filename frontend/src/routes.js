451c04b9213c6315b8490baf638ccf1b25a5d95c
vbueno <viniciusbueno15@gmail.com>
2020-10-23 10:01:33 -0300

    create post

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home';
 import Posts from './pages/singlePost';
+import Create from './pages/createPost';
 
 const Routes = () => (
     <BrowserRouter>
-    <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route path="/posts/:id" component={ Posts } />
-    </Switch>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/posts/:id" component={Posts} />
+            <Route path="/post/new" component={Create} />
+        </Switch>
     </BrowserRouter>
 );
 
