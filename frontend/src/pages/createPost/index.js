09909990fc41eecc6b3da7a2381b9d49ba1d8d39
vbueno <viniciusbueno15@gmail.com>
2020-10-23 10:55:29 -0300

    create style

---
+++ frontend/src/pages/createPost/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import api from '../../services/api';
+
+import './styles.css'
+
+export default function Create() {
+    const [message, setMessage] = useState('');
+
+    const history = useHistory();
+
+    async function newPost(e) {
+        e.preventDefault();
+
+        const data = {
+            message
+        }
+
+        try {
+            await api.post('/posts', data)
+            history.push('/')
+        } catch (error) {
+            alert('error when registering');
+        }
+    }
+
+    function home(e){
+        e.preventDefault();
+        history.push('/')
+    }
+
+    return (
+        <div className="new-incident">
+            <div className="content">
+
+                <form onSubmit={newPost}>
+                    <input
+                        placeholder="New post"
+                        value={message}
+                        onChange={e => setMessage(e.target.value)}
+                        onKeyDown={e => e.keyCode === 13 && setMessage(e.target.value)}
+
+                    />
+                    <button className="button" type="submit">Submit</button>
+                </form>
+            </div>
+            <button className="buttonBack" onClick={home} type="button">Back to Home</button>
+        </div>
+    )
+} 
